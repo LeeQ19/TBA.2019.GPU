e708a8fe40d9e01ad0d23b1789b612f7b89fc596
tgno3 <tgno3.com@gmail.com>
2019-01-10 00:25:10 +0900

    1-input & 3-output model crs output-oriented NVidia vs AMD

--- GPU.R
+++ GPU.R
@@ -15,26 +15,65 @@ sapply(pkgs, require, character.only = T)
 # Load data
 path   <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vRozW_LPzi4Objm4RbLzM_cItbMZzPbaVFheZcv__palp9QhA0qUwUidRqeP7SwrHpcfDSAuXYraPjP/pub?output=csv"
 df.raw <- read.csv(url(path))
+df.raw[, "Released.year"] <- floor(df.raw[, "Released.date"])
 
 # Parameter
 id.out <- c(18, 46, 47, 50, 51, 52, 57, 70, 78, 124, 125, 141, 143, 144, 150, 217, 252, 268)
-id.x   <- c(4:7)
+id.x   <- c(4)
 id.y   <- c(8:10)
-id.t   <- 14
+id.t   <- 15
+fy     <- 2018
+rts    <- "crs"
+ori    <- "o"
 
 
 #####################################################################################
-### Analysis
+### Analysis - all
 #####################################################################################
 
 # Cleansed data
 df.eff  <- df.raw[-id.out,]
 
 # Run
-res.roc <- roc.dea(df.eff[, id.x], df.eff[, id.y], df.eff[, id.t], 7, "vrs", "o")
+res.roc.all <- roc.dea(df.eff[, id.x], df.eff[, id.y], df.eff[, id.t], fy, rts, ori)
 
 # Efficient GPU in 7
-df.eff[round(res.roc$eff_t, 5) == 1,]
+df.eff[round(res.roc.all$eff_t, 5) == 1,]
 
 # RoC
-df.eff[!is.na(res.roc$roc_local),]
+cbind(df.eff, res.roc.all$roc_local)[!is.na(res.roc.all$roc_local),]
+
+
+#####################################################################################
+### Analysis - NVidia
+#####################################################################################
+
+# Selective data
+df.nvd <- subset(df.eff, mf == "NVIDIA")
+
+# Run
+res.roc.nvd <- roc.dea(df.nvd[, id.x], df.nvd[, id.y], df.nvd[, id.t], fy, rts, ori)
+
+# Efficient GPU in 7
+df.nvd[round(res.roc.nvd$eff_t, 5) == 1,]
+
+# RoC
+cbind(df.nvd, res.roc.nvd$roc_local)[!is.na(res.roc.nvd$roc_local),]
+
+
+#####################################################################################
+### Analysis - AMD
+#####################################################################################
+
+# Selective data
+df.amd <- subset(df.eff, mf == "AMD")
+
+# Run
+res.roc.amd <- roc.dea(df.amd[, id.x], df.amd[, id.y], df.amd[, id.t], fy, rts, ori)
+
+# Efficient GPU in 7
+df.amd[round(res.roc.amd$eff_t, 5) == 1,]
+
+# RoC
+cbind(df.amd, res.roc.amd$roc_local)[!is.na(res.roc.amd$roc_local),]
+

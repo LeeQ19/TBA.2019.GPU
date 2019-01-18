71a6e49fbda0fa41ed5d1b8cf8ac732d9f376cb0
LeeQ19 <leekw1994@skku.edu>
2019-01-18 16:32:53 +0900

    make weight vectors bsaed on each output of recent products

--- GPU.R
+++ GPU.R
@@ -9,7 +9,7 @@
 #####################################################################################
 
 # Load library  
-pkgs <- c("DJL")
+pkgs <- c("DJL", "ggplot2")
 sapply(pkgs, require, character.only = T)
 
 # Load data
@@ -94,25 +94,45 @@ month.pred.amd <- 150
 # TDP plot - Nvidia
 ggplot(df.reg.nvd, aes(x = month, y = TDP)) + 
   geom_point() + scale_size(guide = "none") + 
-  geom_smooth(method = 'lm', se = FALSE)
+  geom_smooth(method = 'lm', se = TRUE)
 
 # TDP predict -Nvidia
 model.TDP.nvd <- lm(TDP ~ month, df.reg.nvd)
 summary(model.TDP.nvd)
 
-pred.TDP.nvd <- data.frame(TDP = df.reg.nvd$TDP, pred.TDP = predict(model.TDP.nvd, df.reg.nvd), month = df.reg.nvd$month)
-pred.TDP.nvd <- rbind(pred.TDP.nvd, data.frame(TDP = "", pred.TDP = predict(model.TDP.nvd, data.frame(month = month.pred.nvd)), month = month.pred.nvd))
+pred.TDP.nvd <- data.frame(TDP = df.reg.nvd$TDP, 
+                           pred.TDP = predict(model.TDP.nvd, df.reg.nvd), 
+                           month = df.reg.nvd$month)
+pred.TDP.nvd <- rbind(pred.TDP.nvd, data.frame(TDP = "", 
+                                               pred.TDP = predict(model.TDP.nvd, data.frame(month = month.pred.nvd)), 
+                                               month = month.pred.nvd))
 pred.TDP.nvd
 
 # TDP plot - AMD
 ggplot(df.reg.amd, aes(x = month, y = TDP)) + 
   geom_point() + scale_size(guide = "none") + 
-  geom_smooth(method = 'lm', se = FALSE)
+  geom_smooth(method = 'lm', se = TRUE)
 
 # TDP predict - AMD
 model.TDP.amd <- lm(TDP ~ month, df.reg.amd)
 summary(model.TDP.amd)
 
-pred.TDP.amd <- data.frame(TDP = df.reg.amd$TDP, pred.TDP = predict(model.TDP.amd, df.reg.amd), month = df.reg.amd$month)
-pred.TDP.amd <- rbind(pred.TDP.amd, data.frame(TDP = "", pred.TDP = predict(model.TDP.amd, data.frame(month = month.pred.amd)), month = month.pred.amd))
+pred.TDP.amd <- data.frame(TDP = df.reg.amd$TDP, 
+                           pred.TDP = predict(model.TDP.amd, df.reg.amd), 
+                           month = df.reg.amd$month)
+pred.TDP.amd <- rbind(pred.TDP.amd, data.frame(TDP = "", 
+                                               pred.TDP = predict(model.TDP.amd, data.frame(month = month.pred.amd)), 
+                                               month = month.pred.amd))
 pred.TDP.amd
+
+
+# Weight based on rescaling each output
+weight.nvd <- data.frame(FPP = 1 / df.reg.nvd$Floating.point.performance, 
+                         TR  = 1 / df.reg.nvd$Texture.rate, 
+                         PR  = 1 / df.reg.nvd$Pixel.rate)[nrow(df.reg.nvd), ]
+weight.amd <- data.frame(FPP = 1 / df.reg.amd$Floating.point.performance, 
+                         TR  = 1 / df.reg.amd$Texture.rate, 
+                         PR  = 1 / df.reg.amd$Pixel.rate)[nrow(df.reg.amd), ]
+
+weight     <- rbind(nvd = as.data.frame(t(scale(t(weight.nvd), center = FALSE))), 
+                    amd = as.data.frame(t(scale(t(weight.amd), center = FALSE))))
